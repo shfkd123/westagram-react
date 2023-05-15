396deed5c19a3a88bda2f55a76099c3d7fddddc5
shfkd123 <shfkd123@gmail.com>
2023-05-15 21:20:54 +0900

    class에서 className으로변경

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,30 +4,30 @@ import "./Login.css";
 
 function Login (){
     return(
-    <div class="main">
-      <div class="main-inner">
-          <div class="inner">
-              <div class="inner-header">
+    <div className="main">
+      <div className="main-inner">
+          <div className="inner">
+              <div className="inner-header">
                   <h1>
-                    <span class="lobster">Westagram</span>
+                    <span className="lobster">Westagram</span>
                   </h1>
               </div>
-              <form class="form">
-                  <div class="inputBox">
+              <form className="form">
+                  <div className="inputBox">
                       <input id="id" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" onkeyup='validate()'/>
                   </div>
-                  <div class="inputBox">
+                  <div className="inputBox">
                       <input id="psword" type="password" placeholder="비밀번호" onkeyup='validate()'/>
                   </div>
-                  <div class="loginBtn">
-                      <button type="button" class="disableBtn" id="loginBtn" disabled="true" onclick="login()">
-                          <span class="logintext">로그인</span>
+                  <div className="loginBtn">
+                      <button type="button" className="disableBtn" id="loginBtn" disabled="true" onclick="login()">
+                          <span className="logintext">로그인</span>
                       </button>
                   </div>
               </form>
-              <div class="inner-footer">
-                  {/* <a href="" class="">
-                      <span class="forget">비밀번호를 잊으셨나요?</span>
+              <div className="inner-footer">
+                  {/* <a href="" className="">
+                      <span className="forget">비밀번호를 잊으셨나요?</span>
                   </a> */}
               </div>
            </div>
